a039f844308a67a64fffb5c581124c1ba7f73de4
Bart <barpahl@publicisgroupe.net>
2024-01-26 02:16:36 +0000

    added an array object with other employees and created a for loop to get all their details

--- Staff.js
+++ Staff.js
@@ -1,12 +1,25 @@
 class Employee {
   constructor(name, id, emailAddress) {
-    this.values = [name, id, emailAddress];
+    this.name = name;
+    this.id = id;
+    this.emailAddress = emailAddress;
   }
+
   getEmployeeDetails() {
-    return this.values;
+    return `Employee Name: ${this.name}, ID: ${this.id}, Email Address: ${this.emailAddress}`;
   }
 }
 
 const me = new Employee("Bart", "001", "barpahl@publicis.com");
 
 console.log(me.getEmployeeDetails());
+
+const employees = [
+  new Employee("Kevin", "420", "kevbrownie@publicis.com"),
+  new Employee("Megan", "842", "megshark@publicis.com"),
+  new Employee("Joana", "911", "Joanurkic@digitas.com"),
+];
+
+for (const team of employees) {
+  console.log(team.getEmployeeDetails());
+}
